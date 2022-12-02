abc9dab5b349f5790790640ef96f64e742af6c2b
leonfresh <leonfreshdesign@gmail.com>
2022-12-02 22:39:33 +1100

    changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,20 +67,22 @@ const config = {
           alt: "My Site Logo",
           src: "img/logo-icon.svg",
         },
-        // items: [
-        //   {
-        //     type: "doc",
-        //     docId: "intro",
-        //     position: "left",
-        //     label: "Tutorial",
-        //   },
-        //   { to: "/blog", label: "Blog", position: "left" },
-        //   {
-        //     href: "https://github.com/facebook/docusaurus",
-        //     label: "GitHub",
-        //     position: "right",
-        //   },
-        // ],
+        items: [
+          // {
+          //   type: "doc",
+          //   docId: "intro",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          // { to: "/blog", label: "Blog", position: "left" },
+          // { to: "#preregister", label: "Pre-register", position: "left" },
+          {
+            href: "https://discord.gg/cweD98aeJp",
+            label: "Discord",
+            position: "right",
+            target: "_blank",
+          },
+        ],
       },
       footer: {
         style: "dark",
